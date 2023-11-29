81f1153a0b7975bc3b8178c3abce5b31cb8f8cb1
Puja Naga Prasanna <100214557+puj24@users.noreply.github.com>
2023-11-29 16:43:02 +0530

    Update k_vec_gen.py

--- Miscellaneous/k_vec_gen.py
+++ Miscellaneous/k_vec_gen.py
@@ -1,21 +1,6 @@
 import os
 import glob
 
-    # with open('main.c', 'r') as file :
-    #   filedata = file.read()
-
-
-
-
-    # for filename in glob.glob('*.txt'):
-    #     temp = []
-    #     with open(os.path.join(os.getcwd(), filename), 'r') as f:
-    #         for readline in f:
-    #             for str in readline.split():
-    #                 temp.append(str)
-    #     kvec.append(temp)
-
-filedata =""
 kvec = []
 
 file = open("/mnt/e/Systems/STADS/Electrical/Puja/scripts/kvec_FOV10.csv", "r")
@@ -35,6 +20,3 @@ filedata+=f'int sm_K_vec_arr[N_KVEC_PAIRS][3]={K_vec};\n'
 UISname = f'sm_K_vec_arr.h'
 with open(UISname, 'w') as file :
     file.write(filedata)
-#try:
-#    os.system('touch -m main.c')
-
